f8ab762aba183d5ce2377de6ad298c3be3e8e920
Ral Oliver <raloliver.ws@gmail.com>
2017-10-07 02:02:13 -0300

    finish page details

--- project0/src/app/components/asteroid-details/asteroid-details.component.ts
+++ project0/src/app/components/asteroid-details/asteroid-details.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Asteroid } from '../../models/asteroid';
+import { AsteroidService } from '../../services/asteroid-service.service';
 
 @Component({
   selector: 'asteroid-details',
-  templateUrl: './asteroid-details.component.html'  
+  templateUrl: './asteroid-details.component.html',
+  providers: [AsteroidService]
 })
 export class AsteroidDetailsComponent implements OnInit {
+  asteroid: Asteroid = new Asteroid();
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private courseService: AsteroidService) { }
 
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      let id: number = params['id'];
+      this.getAsteroid(id);
+    });
+  }
+
+  getAsteroid(id: number) {
+    this.courseService.getAsteroid(id)
+      .subscribe(data => this.asteroid = new Asteroid(data),
+      error => console.log(error));
   }
 
 }
