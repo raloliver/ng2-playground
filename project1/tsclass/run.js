9ddfe1b886e73e21f5defa8de1ab5b6e2dc03f1f
Ral Oliver <raloliver.ws@gmail.com>
2017-10-13 13:52:42 -0300

    project1: typescript

---
+++ project1/tsclass/run.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user_1 = require("./user");
+var user = new user_1.User('Israel');
+//# sourceMappingURL=run.js.map
---
+++ project1/tsclass/run.ts
@@ -0,0 +1,3 @@
+import { User } from './user'
+
+let user = new User('Israel')
