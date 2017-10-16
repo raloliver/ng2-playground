9f52edc53bfd768cc1772dac635ace6aae86d39d
Ral Oliver <raloliver.ws@gmail.com>
2017-10-16 14:41:39 -0200

    projetc4: save on ls (localStorage) !

--- project4/src/app/services/data.service.ts
+++ project4/src/app/services/data.service.ts
@@ -8,37 +8,63 @@ export class DataService {
 
   //methods
   constructor() {
-    this.wikis = [
-      {
-        question: 'Como você se chama?',
-        answer: 'Meu nome é João da Silva',
-        show: false
-      },
-      {
-        question: 'Qual a sua idade?',
-        answer: 'Eu tenho 32 anos',
-        show: false
-      },
-      {
-        question: 'Você é casado?',
-        answer: 'Sim',
-        show: false
-      }
-    ]
+    // this.wikis = [
+    //   {
+    //     question: 'Como você se chama?',
+    //     answer: 'Meu nome é João da Silva',
+    //     show: false
+    //   },
+    //   {
+    //     question: 'Qual a sua idade?',
+    //     answer: 'Eu tenho 32 anos',
+    //     show: false
+    //   },
+    //   {
+    //     question: 'Você é casado?',
+    //     answer: 'Sim',
+    //     show: false
+    //   }
+    // ]
   }
 
   //functions
+  //get wikis from ls
   getWikis() {
+    if (localStorage.getItem('wikis') === null) {
+      this.wikis = [];
+    } else {
+      this.wikis = JSON.parse(localStorage.getItem('wikis'));
+    }
     return this.wikis;
   }
 
+  // add wiki from ls
   addWiki(wiki: Wiki) {
     this.wikis.unshift(wiki); //unshift to add on top; push to add on bottom
+
+    // properties (local)
+    let wikis;
+
+    if (localStorage.getItem('wikis') === null) {
+      wikis = [];
+      // push new wiki
+      wikis.unshift(wiki);
+      // new array to ls
+      localStorage.setItem('wikis', JSON.stringify(wikis));
+      
+    } else {
+      wikis = JSON.parse(localStorage.getItem('wikis'));
+      // add new wiki
+      wikis.unshift(wiki);
+      // reset ls
+      localStorage.setItem('wikis', JSON.stringify(wikis));
+    }
   }
 
+  //remove wiki from ls
   removeWiki(wiki: Wiki) {
     for (let i = 0; i < this.wikis.length; i++) {
-      if (wiki == this.wikis[i]){
+      if (wiki == this.wikis[i]) {
         this.wikis.splice(i, 1);
       }
     }
