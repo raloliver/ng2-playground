1a041ba0a5dc02fcf57e27ace0442a3e5923d4c1
Ral Oliver <raloliver.ws@gmail.com>
2017-11-23 16:56:58 -0200

    custom css and first items on home component

--- project5/src/app/home/home.component.ts
+++ project5/src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  goalCount: number = 4
+  btnText: string = 'Incluir na lista'
+
   constructor() { }
 
   ngOnInit() {
