6365a675d9f6cbc1590dbeccd5ff3a12317bcfb7
Ral Oliver <raloliver.ws@gmail.com>
2017-11-23 19:38:23 -0200

    add goal without save

--- project5/src/app/home/home.component.ts
+++ project5/src/app/home/home.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  goalCount: number = 4
+  goalCount: number
   btnText: string = 'Incluir na lista'
+  goalText: string = 'Realizar tudo que me propor a fazer'
+  goals = []
 
   constructor() { }
 
   ngOnInit() {
+    this.goalCount = this.goals.length
+  }
+
+  addGoal() {
+    this.goals.push(this.goalText)
+    this.goalText = ''
+    this.goalCount = this.goals.length
   }
 
 }
