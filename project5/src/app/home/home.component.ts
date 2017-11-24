d369c64f1d980c2bd9216fbfa669b29b2c3aace5
Ral Oliver <raloliver.ws@gmail.com>
2017-11-24 11:38:25 -0200

    add animations on add an del goals

--- project5/src/app/home/home.component.ts
+++ project5/src/app/home/home.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
+          ]))]), { optional: true }),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
+          ]))]), { optional: true })
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   goalCount: number
   btnText: string = 'Incluir na lista'
-  goalText: string = 'Realizar tudo que me propor a fazer'
-  goals = []
+  goalText: string = 'Começar tudo isso hoje mesmo'
+  goals = ["Realizar tudo que me propor a fazer", "Definir o que de fato é importante pra mim", "Aprender a aprender"]
+
 
   constructor() { }
 
@@ -19,9 +42,13 @@ export class HomeComponent implements OnInit {
   }
 
   addGoal() {
-    this.goals.push(this.goalText)
+    this.goals.unshift(this.goalText)
     this.goalText = ''
     this.goalCount = this.goals.length
+  }
+
+  delGoal(i) {
+    this.goals.splice(i, 1)
   }
 
 }
