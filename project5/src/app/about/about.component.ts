7c0060ebfeba45ed7223c6f7cc95e5d11645a9a0
Ral Oliver <raloliver.ws@gmail.com>
2017-11-27 17:13:10 -0200

    add router to about component

--- project5/src/app/about/about.component.ts
+++ project5/src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -9,11 +8,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe(res => console.warn(res.id))
   }
 
   ngOnInit() {
+  }
+
+  sendMeHome() {
+    this.router.navigate([''])
   }
 
 }
